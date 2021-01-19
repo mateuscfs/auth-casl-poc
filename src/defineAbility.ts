954400bfb0626bc76f94a5256e527e82ed2be1d5
Mateus Coelho <mateuscfs@gmail.com>
2021-01-19 17:10:48 -0300

    fix: Merge Fix

--- src/defineAbility.ts
+++ src/defineAbility.ts
@@ -1,5 +1,7 @@
 import { defineAbility } from '@casl/ability';
 
+//First Way
+
 export default (user: Record<string, unknown>) => defineAbility((can, cannot) => {
   cannot('Generate File', 'ecdservice');
   can('List', 'documentservice');
@@ -8,3 +10,42 @@ export default (user: Record<string, unknown>) => defineAbility((can, cannot) =>
       can('Generate File', 'ecdservice', { author: user.name })
   }
 });
+
+// Second Way
+
+export const defineAbilities = (user: Record<string, unknown>, header: Record<string, unknown>) => {
+    return defineAbility((can, cannot) => {
+        cannot('Generate', 'fiscal.ecdservice.file');
+        cannot('ReadAll', 'dfe.documentservice.NFe');
+        can('Create', 'dfe.documentservice.NFe');
+
+        if (user.type == 'admin') {
+            can('Generate', 'fiscal.ecdservice.file');
+            can('Generate', 'fiscal.*');
+        }
+
+        switch (header.module) {
+          case 'Fiscal':
+              can('ReadAll', 'dfe.documentservice.NFe');
+              break;
+          case 'Retail':
+              can('Read', 'retail.taxservice.NFe');
+              break;
+          case 'DFe':
+              can('ReadAll', 'commmon.generalservice.groups');
+              break;
+        }
+    }); 
+};
+
+// Actions
+
+/* 
+    Create
+    Update
+    Delete
+    Execute
+    Read
+    ReadAll
+    *
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-import defineAbility from './defineAbility';
+import { defineAbilities } from './defineAbility';
 import rules from './getAbility';
 
-const user = { name: 'Mateus' };
-const ability = defineAbility(user);
-
-console.log(ability.can('Generate File', 'ecdservice')); // true
-console.log(ability.can('List', 'documentservice')); // true
-console.log(ability.can('Create', 'documentservice')); // false
+// First Way
 
 const request = {
     url: '/fiscal/create',
@@ -17,6 +12,7 @@ const request2 = {
     url: '/fiscal/delete',
     module: 'fiscal',
 };
+
 
 const request3 = {
     url: '/fiscal/execute',
@@ -32,3 +28,23 @@ console.log(rules.can(request.url, request.module)); // true
 console.log(rules.can(request2.url, request2.module)); // true
 console.log(rules.can(request3.url, request3.module)); // true
 console.log(rules.can(request4.url, request4.module)); // false
+
+// Second Way
+
+const user = { name: 'Mateus', type: 'admin' };
+const header = { module: 'Fiscal'}
+
+const testAbility = defineAbilities({ type: 'common' }, { module: 'Retail' })
+
+console.log(testAbility.can('Generate', 'fiscal.ecdservice.file')); // false
+console.log(testAbility.can('ReadAll', 'dfe.documentservice.NFe')); // false
+console.log(testAbility.can('Create', 'dfe.documentservice.NFe')); // true
+
+const testAbility2 = defineAbilities(user, header)
+
+console.log(testAbility2.can('Generate', 'fiscal.ecdservice.file')) // true
+console.log(testAbility2.can('ReadAll', 'dfe.documentservice.NFe')) // true
+console.log(testAbility2.can('Create', 'dfe.documentservice.NFe')) // true
+console.log(testAbility2.can('Read', 'retail.taxservice.NFe')) // false
+console.log(testAbility2.can('Generate', 'fiscal.*')) // true
+console.log(testAbility2.can('Generate', 'fiscal.ecdservice.*')) // false
