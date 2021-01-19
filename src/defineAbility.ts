1523a0f516a78cd2450b82d150aad30a18a97a41
Mateus Coelho <mateuscfs@gmail.com>
2021-01-19 11:00:48 -0300

    feat: added another way to implement

--- src/defineAbility.ts
+++ src/defineAbility.ts
@@ -1,10 +1,52 @@
-import { defineAbility } from '@casl/ability';
+import { Ability, defineAbility } from '@casl/ability';
 
-export default (user: Record<string, unknown>) => defineAbility((can, cannot) => {
-  cannot('Generate File', 'ecdservice');
-  can('List', 'documentservice');
-  cannot('Create', 'documentservice');
-  if (user.name) {
-      can('Generate File', 'ecdservice', { author: user.name })
-  }
-});
+// First Way
+
+/* export default (user: Record<string, unknown>, header: Record<string, unknown>) => defineAbility((can, cannot) => {
+    cannot('Generate', 'fiscal.ecdservice.file');
+    cannot('ReadAll', 'dfe.documentservice.NFe');
+    cannot('Create', 'dfe.documentservice.NFe');
+
+    if (user.type == 'admin') {
+        can('Generate', 'fiscal.ecdservice.file');
+    }
+
+    switch (header.module) {
+      case 'Fiscal':
+          can('ReadAll', 'dfe.documentservice.NFe');
+          break;
+      case 'Retail':
+          can('Read', 'retail.taxservice.NFe');
+          break;
+      case 'DFe':
+          can('ReadAll', 'commmon.generalservice.groups');
+          break;
+    }
+
+}); */
+
+// Second Way
+
+export default new Ability ([
+    {
+        action: 'Generate',
+        subject: 'fiscal.ecdservice.file',
+        inverted: true,
+    },
+    {
+        action: 'ReadAll',
+        subject: 'dfe.documentservice.NFe',
+    },
+])
+
+// Actions
+
+/* 
+    Create
+    Update
+    Delete
+    Execute
+    Read
+    ReadAll
+    *
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
+import Ability from './defineAbility';
 import defineAbility from './defineAbility';
 
-const user = { name: 'Mateus' }
-const ability = defineAbility(user)
+const user = { type: 'admin' }
+const header = { module: 'Fiscal' }
 
-console.log(ability.can('Generate File', 'ecdservice')) // true
-console.log(ability.can('List', 'documentservice')) // true
-console.log(ability.can('Create', 'documentservice')) // false
+// First Way
+
+/* const testAbility = defineAbility({ type: 'common' }, { module: 'Retail' })
+
+console.log(testAbility.can('Generate', 'fiscal.ecdservice.file')) // false
+console.log(testAbility.can('ReadAll', 'dfe.documentservice.NFe')) // false
+console.log(testAbility.can('Create', 'dfe.documentservice.NFe')) // false
+console.log(testAbility.can('Read', 'retail.taxservice.NFe')) // true
+
+const ability = defineAbility(user, header)
+
+console.log(ability.can('Generate', 'fiscal.ecdservice.file')) // true
+console.log(ability.can('ReadAll', 'dfe.documentservice.NFe')) // true
+console.log(ability.can('Create', 'dfe.documentservice.NFe')) // false
+console.log(ability.can('Read', 'retail.taxservice.NFe')) // false
+ */
+
+// Second Way
+
+console.log(Ability.can('Generate', 'fiscal.ecdservice.file')) // false
+console.log(Ability.can('ReadAll', 'dfe.documentservice.NFe')) // true
+console.log(Ability.can('Create', 'dfe.documentservice.NFe')) // true
